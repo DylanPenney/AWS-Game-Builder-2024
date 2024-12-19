940b801b4c3b0f49d04306181ab8edeac3738a29
Dylan Penney <penneydylan@icloud.com>
2024-12-19 22:10:33 +0000

    chat is this real

---
+++ game/src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import { Game } from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Horse Racing Game</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
