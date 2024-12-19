a8ab9ead5aa9420133d26ef464fce0e8e4892814
Dylan Penney <penneydylan@icloud.com>
2024-12-19 23:36:41 +0000

    deleted redunant src folder

--- src/utils/gameUtils.ts
+++
@@ -1,5 +0,0 @@
-// src/utils/gameUtils.ts
-export const calculateWPM = (text: string, timeElapsed: number): number => {
-    const words = text.trim().split(/\s+/).length
-    return words / timeElapsed
-}
