6763081616bb7d7548058f31bb4a2085916cc668
Dylan's CI Autoformatter <runner@fv-az810-57.xqqg2nvqhtvu3bmcaca3aq1dmh.cx.internal.cloudapp.net>
2024-12-20 15:01:33 +0000

    Automated changes by prettier

--- game/src/index.js
+++ game/src/index.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
-const controls = ReactDOM.createRoot(document.getElementById('controls'))
+const controls = ReactDOM.createRoot(document.getElementById("controls"));
 controls.render(
-    <React.StrictMode>
-        <div className="controls">
-            <div className="controls__title">Control Pannel</div>
-            <div className="controls__description">
-                Customise Game with the following options
-            </div>
-        </div>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <div className="controls">
+      <div className="controls__title">Control Pannel</div>
+      <div className="controls__description">
+        Customise Game with the following options
+      </div>
+    </div>
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
