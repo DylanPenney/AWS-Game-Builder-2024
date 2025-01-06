b230a0cd0de2e2f36132e1fcb7333ee0a8e863ed
Dylan's CI Autoformatter <runner@fv-az1344-816.11nhhp5tf3ce1ixkodulnhsnqg.dx.internal.cloudapp.net>
2025-01-06 16:28:56 +0000

    Automated changes by prettier

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,30 +1,30 @@
 // src/App.tsx
-import React from 'react'
-import { Game } from './components/Game'
-import { Header } from './components/Header'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Game } from "./components/Game";
+import { Header } from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    const selectedPage = 'Play'
+  const selectedPage = "Play";
 
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/play" element={<Game />} />
-                    <Route path="/settings" element={<Game />} />
-                    <Route path="/help" element={<Game />} />
-                    <Route path="/high-scores" element={<Game />} />
-                    <Route path="/dev-menu" element={<Game />} />
-                </Routes>
-            </BrowserRouter>
-            <Game />
-            <div>
-                <span>{selectedPage}</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/play" element={<Game />} />
+          <Route path="/settings" element={<Game />} />
+          <Route path="/help" element={<Game />} />
+          <Route path="/high-scores" element={<Game />} />
+          <Route path="/dev-menu" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
+      <Game />
+      <div>
+        <span>{selectedPage}</span>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- game/src/components/Header.tsx
+++ game/src/components/Header.tsx
@@ -1,54 +1,52 @@
-import React from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import '../styles/Header.css'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "../styles/Header.css";
 
-const BUTTONS = ['Play', 'Settings', 'Help']
-const BUTTONS_DEV = ['High Scores', 'Dev Menu']
+const BUTTONS = ["Play", "Settings", "Help"];
+const BUTTONS_DEV = ["High Scores", "Dev Menu"];
 
 const DEV =
-    window.location.hostname === 'localhost' ||
-    window.location.hostname === '127.0.0.1'
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
 
 export const Header: React.FC = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function selectPage({ button }: { button: string }) {
-        return () => {
-            // set the selectedPage to the button that was clicked
-            // if the button is already selected, do nothing
-            // and ensure the app can access this variable
+  function selectPage({ button }: { button: string }) {
+    return () => {
+      // set the selectedPage to the button that was clicked
+      // if the button is already selected, do nothing
+      // and ensure the app can access this variable
 
-            navigate(`/${button.toLowerCase()}`)
-        }
-    }
+      navigate(`/${button.toLowerCase()}`);
+    };
+  }
 
-    if (DEV && !BUTTONS.includes(BUTTONS_DEV[0])) {
-        BUTTONS_DEV.forEach((button: string) => {
-            BUTTONS.push(button)
-        })
-    }
-    return (
-        <div id="menu">
-            <img
-                src="/favicon.png"
-                alt="logo.png"
-                onClick={selectPage({ button: 'Play' })}
-            />
+  if (DEV && !BUTTONS.includes(BUTTONS_DEV[0])) {
+    BUTTONS_DEV.forEach((button: string) => {
+      BUTTONS.push(button);
+    });
+  }
+  return (
+    <div id="menu">
+      <img
+        src="/favicon.png"
+        alt="logo.png"
+        onClick={selectPage({ button: "Play" })}
+      />
 
-            <div id="headerButtonsContainer">
-                {BUTTONS.map((button) => (
-                    <div className="buttonContainer" key={button}>
-                        <NavLink
-                            to={`/${button.toLowerCase()}`}
-                            className={({ isActive }) =>
-                                isActive ? 'selected' : ''
-                            }
-                        >
-                            {button}
-                        </NavLink>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
+      <div id="headerButtonsContainer">
+        {BUTTONS.map((button) => (
+          <div className="buttonContainer" key={button}>
+            <NavLink
+              to={`/${button.toLowerCase()}`}
+              className={({ isActive }) => (isActive ? "selected" : "")}
+            >
+              {button}
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
